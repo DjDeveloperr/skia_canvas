3c4e7da9f08e1a61afa8a33993dcfb0ed89b89c0
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-02-18 13:33:15 -0800

    chore: modernize module

--- deps.ts
+++ deps.ts
@@ -1,16 +1,17 @@
-export { dlopen } from "https://deno.land/x/plug@1.0.0-rc.3/mod.ts";
+export { dlopen } from "https://deno.land/x/plug@1.0.3/mod.ts";
 export {
   createDownloadURL,
   ensureCacheLocation,
-} from "https://deno.land/x/plug@1.0.0-rc.3/download.ts";
-export {
-  isFile,
-  urlToFilename,
-} from "https://deno.land/x/plug@1.0.0-rc.3/util.ts";
-export type { FetchOptions } from "https://deno.land/x/plug@1.0.0-rc.3/mod.ts";
+} from "https://deno.land/x/plug@1.0.3/download.ts";
+export { isFile, urlToFilename } from "https://deno.land/x/plug@1.0.3/util.ts";
+export type { FetchOptions } from "https://deno.land/x/plug@1.0.3/mod.ts";
 export {
   dirname,
   extname,
   join,
-} from "https://deno.land/std@0.159.0/path/mod.ts";
-export { ensureDir } from "https://deno.land/std@0.159.0/fs/mod.ts";
+} from "https://deno.land/std@0.216.0/path/mod.ts";
+export { ensureDir } from "https://deno.land/std@0.216.0/fs/ensure_dir.ts";
+export {
+  decodeBase64,
+  encodeBase64,
+} from "https://deno.land/std@0.216.0/encoding/base64.ts";
